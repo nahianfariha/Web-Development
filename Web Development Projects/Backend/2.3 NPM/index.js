bdc225e366b4a23e390d59544306299f110184ff
NahianFariha <nahianfarihaofficial@gmail.com>
2025-02-18 12:47:27 +0600

    commit

---
+++ Web Development Projects/Backend/2.3 NPM/index.js
@@ -0,0 +1,8 @@
+/* var generateName = require('sillyname');
+var sillyName = generateName();
+console.log("My name is " + sillyName); */
+
+import {randomSuperhero} from 'superheroes';
+
+randomSuperhero();
+console.log("I am " + randomSuperhero());
